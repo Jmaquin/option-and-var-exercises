113d13606877ae8e071a8e8504e1e6406d3a0028
Johann Maquin <johann.maquin@gmail.com>
2025-01-24 17:41:50 +0100

    Implement Black&Scholes model for Call and Put options

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# option-and-VaR-exercises
-Implementation of Black&amp;Scholes model for vanilla option pricing and Value at Risk methodology
+# Option and Value at Risk exercises
+## 1)Option
+Implement the Black and Scholes model for Call and Put option pricing
+
+### a)Call Option Pricing Formula (Black-Scholes)
+The Black-Scholes formula for pricing a European Call Option is:
+
+$$ C = S \cdot N(d_1) - K \cdot e^{-rT} \cdot N(d_2) $$
+
+#### where:
+$$ d_1 = \frac{\ln(S / K) + (r + \sigma^2 / 2)T}{\sigma \sqrt{T}} $$
+$$ d_2 = d_1 - \sigma \sqrt{T} $$
+
+### b)Put Option Pricing Formula (Black-Scholes)
+The Black-Scholes formula for pricing a European Put Option is:
+
+$$ P = K \cdot e^{-rT} \cdot N(-d_2) - S \cdot N(-d_1) $$
+
+#### where:
+$$ d_1 = \frac{\ln(S / K) + (r + \sigma^2 / 2)T}{\sigma \sqrt{T}} $$
+$$ d_2 = d_1 - \sigma \sqrt{T} $$
+
+#### Parameters:
+
+- **S**: Current stock price at the time of pricing(spot price).
+- **K**: Strike price — the price at which the option can be exercised.
+- **r**: Risk-free interest rate, expressed as a decimal.
+- **T**: Time to expiration, in years.
+- **σ (sigma)**: Volatility of the stock, expressed as a decimal.
+- **N(x)**: The cumulative distribution function of the standard normal distribution.
+
---
+++ tests/test_black_scholes.py
@@ -0,0 +1,112 @@
+import math
+
+import pytest
+from numpy.ma.testutils import assert_equal
+from scipy.stats import norm
+
+from src.black_scholes import BlackScholesModel
+
+# Given
+test_params = {
+    "spot_price": 100,
+    "strike_price": 100,
+    "time_to_maturity": 1,
+    "risk_free_rate": 0.05,
+    "volatility": 0.2,
+}
+
+
+def test_calculate_d1():
+    # When
+    result = BlackScholesModel.calculate_d1(
+        test_params["spot_price"],
+        test_params["strike_price"],
+        test_params["time_to_maturity"],
+        test_params["risk_free_rate"],
+        test_params["volatility"],
+    )
+
+    # Then
+    expected_d1 = (math.log(test_params["spot_price"] / test_params["strike_price"]) +
+                   (test_params["risk_free_rate"] + (test_params["volatility"] ** 2) / 2) *
+                   test_params["time_to_maturity"]) / (
+                          test_params["volatility"] * math.sqrt(test_params["time_to_maturity"]))
+    assert_equal(result, expected_d1)
+
+
+def test_calculate_d2():
+    # Given
+    d1 = 0.5
+
+    # When
+    result = BlackScholesModel.calculate_d2(d1, test_params["volatility"], test_params["time_to_maturity"])
+
+    # Then
+    expected_d2 = d1 - test_params["volatility"] * math.sqrt(test_params["time_to_maturity"])
+    assert_equal(result, expected_d2)
+
+
+def test_call_option_price():
+    # Given
+    d1 = BlackScholesModel.calculate_d1(
+        test_params["spot_price"],
+        test_params["strike_price"],
+        test_params["time_to_maturity"],
+        test_params["risk_free_rate"],
+        test_params["volatility"],
+    )
+    d2 = BlackScholesModel.calculate_d2(d1, test_params["volatility"], test_params["time_to_maturity"])
+
+    # When
+    result = BlackScholesModel().call_option_price(**test_params)
+
+    # Then
+    expected_call_price = (test_params["spot_price"] * norm.cdf(d1) -
+                           test_params["strike_price"] * math.exp(
+                -test_params["risk_free_rate"] * test_params["time_to_maturity"]) * norm.cdf(d2))
+    assert_equal(result, expected_call_price)
+
+
+def test_put_option_price():
+    # Given
+    d1 = BlackScholesModel.calculate_d1(
+        test_params["spot_price"],
+        test_params["strike_price"],
+        test_params["time_to_maturity"],
+        test_params["risk_free_rate"],
+        test_params["volatility"],
+    )
+    d2 = BlackScholesModel.calculate_d2(d1, test_params["volatility"], test_params["time_to_maturity"])
+
+    # When
+    result = BlackScholesModel().put_option_price(**test_params)
+
+    # Then
+    expected_put_price = (test_params["strike_price"] * math.exp(
+        -test_params["risk_free_rate"] * test_params["time_to_maturity"]) * norm.cdf(-d2) -
+                          test_params["spot_price"] * norm.cdf(-d1))
+    assert_equal(result, expected_put_price)
+
+
+def test_d1_with_zero_volatility():
+    with pytest.raises(ZeroDivisionError):
+        BlackScholesModel.calculate_d1(
+            test_params["spot_price"],
+            test_params["strike_price"],
+            test_params["time_to_maturity"],
+            test_params["risk_free_rate"],
+            0,
+        )
+
+
+def test_d2_with_zero_volatility():
+    # Given
+    d1 = 0.5
+    volatility = 0
+
+    # When
+    result = BlackScholesModel.calculate_d2(d1, volatility, test_params["time_to_maturity"])
+
+    # Then
+    expected_d2 = d1
+    assert_equal(result, expected_d2)
